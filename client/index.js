f43e4abe237f90aec810c8220bd458c0a2c2df16
Jacob Bass <jacob@jacobbass.net>
2016-02-28 17:11:14 +1100

    add playground start

--- client/index.js
+++ client/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 
 import styles from './index.scss';
 
-console.log(styles)
-
 class HelloWorld extends Component {
   render() {
     return (
@@ -14,9 +12,29 @@ class HelloWorld extends Component {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    const fixtures = [
+      { greeting: 'Hello', target: 'Jacob' },
+      { greeting: 'Hello', target: 'World' },
+    ];
+
+    this.fixtures = fixtures;
+    this.state = { active: 0 };
+  }
   render() {
+    const fixtureLinks = this.fixtures.map((f, i) => {
+      return <li className={this.state.active === i ? styles.active : ''} key={JSON.stringify(f)}><div onClick={() => { this.setState({active: i})}}>{JSON.stringify(f)}</div></li>
+    });
+
     return (
-      <HelloWorld greeting={'Hello'} target={'Jacob'}/>
+      <div>
+        <div>
+          <ul>{fixtureLinks}</ul>
+        </div>
+        <HelloWorld {...this.fixtures[this.state.active]} />
+      </div>
     );
   }
 }
