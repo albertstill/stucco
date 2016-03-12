90633773aec176e46982900f7ffb94f39fe8ed58
Jacob Bass <jacob@jacobbass.net>
2016-03-12 14:58:14 +1100

    remove example from git

--- example/fixtures.js
+++
@@ -1,6 +0,0 @@
-const fixtures = [
-  { label: '01', props: { greeting: 'Hello', target: '01World' } },
-  { label: '02', props: { greeting: 'Hello', target: '02World' } },
-];
-
-export default fixtures;
--- example/routes.js
+++
@@ -1,9 +0,0 @@
-const routes = [
-  {
-    method: 'get',
-    pattern: '/hello',
-    handler: (req, res) => res.send('hello'),
-  },
-];
-
-export default routes;
