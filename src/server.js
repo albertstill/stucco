612af98b8bfa6a7285a39d8a50f540771f948e73
Albert Still <albert.still@domain.com.au>
2016-07-04 12:56:05 +1000

    exclude dependency folders from using the babel loader

--- src/server.js
+++ src/server.js
@@ -26,6 +26,7 @@ function generateWebpackCompiler(entryPath) {
       loaders: [
         {
           test: /\.js?$/,
+          exclude: /(node_modules|bower_components)/,
           loader: 'babel',
           query: {
             presets: ['react', 'es2015'],
